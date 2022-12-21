aacc7254f1cc119b61d856c13389a2fdb758260e
Lbrt64 <lbruyant@outlook.fr>
2022-12-21 16:47:14 +0100

    State saved in local storage with redux persist

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,18 @@ import { RouterProvider } from "react-router-dom";
 import { router } from './utils/router/router';
 import { Provider } from "react-redux";
 import { store } from './utils/store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-      <React.StrictMode>
-        <RouterProvider router={router} />
-      </React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+        <React.StrictMode>
+          <RouterProvider router={router} />
+        </React.StrictMode>
+    </PersistGate>
   </Provider>
 );
--- src/utils/store/store.js
+++ src/utils/store/store.js
@@ -1,9 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userTokenReducer from './userTokenSlice'
+import storage from 'redux-persist/lib/storage'
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import { combineReducers } from '@reduxjs/toolkit'
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage
+}
+
+const reducer = combineReducers({
+    userToken: userTokenReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 export const store = configureStore({
-    reducer: {
-            userToken: userTokenReducer
-        }
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
 })
 
