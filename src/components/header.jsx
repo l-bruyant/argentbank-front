b2d46c9aa29be6893645cad22d17d642208d1894
Lbrt64 <lbruyant@outlook.fr>
2023-01-10 10:24:08 +0100

    Added user name to nav bar when user is connected

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import logoArgentBank from '../assets/img/argentBankLogo.png'
 import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { tokenRemove } from '../utils/store/userTokenSlice'
+import { requestUserName } from '../utils/api/api'
 
 /**
 *
 * Displays the page Header
 * If the user has no identification token : displays the login button
-* If the user has an identification token : displays my account and sign out buttons
+* If the user has an identification token : displays user name and sign out buttons
 *
 * @function Header
 *
@@ -16,8 +17,20 @@ import { tokenRemove } from '../utils/store/userTokenSlice'
 
 export default function Header () {
     const userToken = useSelector(state => state.userToken.value)
+    const [userFirstName, setUserFirstName] = useState()
     const userHasToken = userToken !== null
     const dispatch = useDispatch()
+
+    useEffect (() => {
+        if (userHasToken) {
+            const call = async () => {
+                const res = await requestUserName(userToken)
+                setUserFirstName(res.body.firstName)
+            }
+            call()
+        }
+    }, [userToken] ) 
+
 
     return (
         <nav className="main-nav">
@@ -36,7 +49,7 @@ export default function Header () {
                             return (isActive ? 'main-nav-item active-nav' : 'main-nav-item')
                         }}>
                             <i className="fa fa-user-circle" />
-                            Account
+                            {userFirstName}
                         </NavLink>
                     </div>
                     <div>
